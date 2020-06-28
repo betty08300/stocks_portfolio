b8bc8f92212f2db6211bc3b3100052f87942ab60
Betty Wren <wreny641@gmail.com>
2020-06-28 17:31:36 -0400

    add docker

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectedDatabase = async() => {
-    return await mongoose.connect('mongodb://localhost:27017/stocks', {useNewUrlParser: true, useUnifiedTopology: true});
+    return await mongoose.connect('mongodb://mongo:27017/stocks', {useNewUrlParser: true, useUnifiedTopology: true});
 }
 
 module.exports = connectedDatabase; 
--- backend/index.js
+++ backend/index.js
@@ -8,36 +8,36 @@ const userRouter = require('./routes/user');
 const dashboardRouter = require('./routes/dashboard');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const path = require('path'); 
+const path = require('path');
 
 const seed = async () => {
     const db = await connectDatabase();
     try {
         await User.collection.drop();
-    } catch (error){
-        console.log(error.errmsg);
+    } catch (error) {
+        console.log(error);
     }
-    const user = await User.createAuthUser({name:'Betty', email:'betty@gmail.com', password:'123456'});
-     await user.buy ({
+    const user = await User.createAuthUser({ name: 'Betty', email: 'betty@gmail.com', password: '123456' });
+    await user.buy({
         company: 'Alphabet, Inc.',
         ticker: 'GOOG',
         price: 20,
         share: 5
     });
 
-    await user.buy ({
+    await user.buy({
         company: 'Apple, Inc.',
         ticker: 'AAPL',
         price: 50,
         share: 10
     });
-    await user.buy ({
+    await user.buy({
         company: 'Alphabet, Inc.',
         ticker: 'GOOG',
         price: 30,
         share: 4
     });
-    await user.buy ({
+    await user.buy({
         company: 'Facebook, Inc.',
         ticker: 'FB',
         price: 1,
@@ -52,8 +52,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use('/user', userRouter);
